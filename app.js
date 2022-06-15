2e89c2cfd4d493e0dc46c1359bb371cf3006bbdf
Guillaume LeDevin <guillaume.ledevin@personalgroup.com>
2022-06-15 07:17:43 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const longMountain = document.querySelector('.home-mountains img');
+const title = document.querySelector('h1');
+
+gsap.to(longMountain, {
+    y: 100,
+    scrollTrigger: {
+        trigger: 'home-mountains',
+        scrub: true
+    }
+})
+
+gsap.to(title, {
+    y: 500,
+    scrollTrigger: {
+        trigger: 'home-mountains',
+        scrub: true
+    }
+})
